b8913fd0161d54eb75bfa459d0cde102f578d252
blameazu <85005499+blameazu@users.noreply.github.com>
2025-03-28 11:12:40 +0800

    Update gen.cpp

--- pA/gen/gen.cpp
+++ pA/gen/gen.cpp
@@ -5,5 +5,5 @@ int main(int argc, char* argv[]) {
 	registerGen(argc, argv, 1);
 	int q = rnd.next(100000, 1000000);
 	cout << q << '\n';
-	while(q--) cout << rnd.next(1, 1000000000) << " \n"[!q];
+	while(q--) cout << rnd.next(1, 3) << ' ' << rnd.next(1, 1000000000) << '\n';
 }
