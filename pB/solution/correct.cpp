7b80bc98cd9db61ca90fd22dd0ab43c7a7e2a810
blameazu <85005499+blameazu@users.noreply.github.com>
2025-03-28 11:17:17 +0800

    Update correct.cpp

--- pB/solution/correct.cpp
+++ pB/solution/correct.cpp
@@ -1,10 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
 using namespace std;
-
 int main() {
-	// TODO
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+	int q; cin >> q;
+	map<int, int> mp;
+	while(q--) {
+		int a, b; cin >> a >> b;
+		if(a == 1) mp[b]++;
+		else if(a == 2) mp[b] = max(0, mp[b]-1);
+		else cout << (mp[b] ? mp[b] : -1) << '\n';
+	}
 }
