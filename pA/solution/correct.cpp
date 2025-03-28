4857d2b459e9e4ba65d6ee481b921c81bb579b3d
blameazu <85005499+blameazu@users.noreply.github.com>
2025-03-28 10:48:18 +0800

    add pA solution
    haha

--- pA/solution/correct.cpp
+++ pA/solution/correct.cpp
@@ -1,10 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
 using namespace std;
-
 int main() {
-	// TODO
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+	set<int> se;
+	int q; cin >> q;
+	while(q--) {
+		int c, a; cin >> c >> a;
+		if(c == 1) se.insert(a);
+		else if(c == 2) {if(se.find(a) != se.end()) se.erase(a);}
+		else cout << (se.find(a) != se.end() ? "No\n" : "Yes\n");
+	}
 }
