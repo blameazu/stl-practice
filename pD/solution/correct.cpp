5518fc6811a63d0c0225080376dad06a1c8c8ff8
blameazu <85005499+blameazu@users.noreply.github.com>
2025-04-18 11:28:01 +0800

    Update correct.cpp

--- pD/solution/correct.cpp
+++ pD/solution/correct.cpp
@@ -1,10 +1,26 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
 using namespace std;
-
 int main() {
-	// TODO
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+	int q; cin >> q;
+	map<string, int> mp;
+	while(q--) {
+		int c; cin >> c;
+		if(c == 1) {
+			string a; int b; cin >> a >> b;
+			if(mp.count(a)) cout << "Already Exist!\n";
+			else mp[a] = b;
+		} else if(c == 2) {
+			string a; cin >> a;
+			mp.erase(a);
+		} else if(c == 3) {
+			string a; int b; cin >> a >> b;
+			if(!mp.count(a)) cout << a << " is not exist!\n";
+			else mp[a] = b;
+		} else {
+			string a; cin >> a;
+			if(!mp.count(a)) cout << a << " is not exist!\n";
+			else cout << mp[a] << '\n';
+		}
+	}
 }
