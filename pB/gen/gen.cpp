d2630ed8c1bed9db3247fd13edd31fbd0728720d
blameazu <85005499+blameazu@users.noreply.github.com>
2025-04-18 11:30:51 +0800

    Update gen.cpp

--- pB/gen/gen.cpp
+++ pB/gen/gen.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(int argc, char* argv[]) {
 	registerGen(argc, argv, 1);
-	int n = rnd.next(1, 1000000);
+	int n = rnd.next(1, atoi(argv[1]));
 	cout << n << '\n';
 	while(n--) {
 		cout << rnd.next(1, 3) << ' ' << rnd.next(1, 1000000000) << '\n';
